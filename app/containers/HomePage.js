ab7758f696b8cf76518e573ee7d39fa3401eb720
Toby Yuan <1055410108@qq.com>
2018-01-23 20:56:14 +0800

    添加antd fix样式失效bug

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,7 +1,71 @@
 import React, { Component } from 'react'
+import { Layout, Menu, Icon } from 'antd'
+import styles from '../assets/css/homePage.css'
+
+const { Header, Sider, Content } = Layout
 
 export default class HomePage extends Component {
+    state = {
+        collapsed: false
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
     render() {
-        return <div>HomePage</div>
+        return (
+            <Layout style={{ height: '100vh' }}>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className={styles.logo} />
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                    >
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            className={styles.trigger}
+                            type={
+                                this.state.collapsed
+                                    ? 'menu-unfold'
+                                    : 'menu-fold'
+                            }
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                            minHeight: 280
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        )
     }
 }
