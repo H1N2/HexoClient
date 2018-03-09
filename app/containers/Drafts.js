f0d3c1df5a3e3389a9df6c3c6f5ccf4c66caea2e
TangGe1119 <1055410108@qq.com>
2018-03-09 11:15:46 +0800

    初步完成七牛上传功能

--- app/containers/Drafts.js
+++ app/containers/Drafts.js
@@ -5,11 +5,22 @@ import { use } from '../service'
 
 class Drafts extends Component {
     render() {
-        return <CommonListPage type="draft" baseDir={this.props.baseDir} />
+        return (
+            <CommonListPage
+                type="draft"
+                baseDir={this.props.baseDir}
+                ak={this.props.ak}
+                sk={this.props.sk}
+                bucket={this.props.bucket}
+            />
+        )
     }
 }
 const mapStateToProps = state => ({
-    baseDir: state.system.baseDir
+    baseDir: state.system.baseDir,
+    ak: state.system.ak,
+    sk: state.system.sk,
+    bucket: state.system.bucket
 })
 
 export default connect(mapStateToProps)(Drafts)
